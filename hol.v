a49adab789b5c466208ce0868d058b38efeddbec
Nicolas Marti <nicolas.marti.japon@gmail.com>
2013-02-27 23:22:04 +0900

    continue up to before subst rules ...

--- hol.v
+++ hol.v
@@ -3,7 +3,7 @@ Axiom bool: Set.
 
 (* from bool to Prop *)
 Axiom judgment: bool -> Prop.
-Notation " |- x" := (judgment x) (at level 95, no associativity).
+Notation "|-  x" := (judgment x) (at level 95, no associativity).
 
 (* basic primitives: equality, implication and choice *)
 Axiom eq: forall {A: Set}, A -> A -> bool.
@@ -17,7 +17,7 @@ Axiom choice: forall {A: Set}, (A -> bool) -> A.
 (* *)
 
 (* the deduction system axioms *)
-Axiom eq_refl: forall {A: Set} (a: A), a = a.
+Axiom eq_refl: forall {A: Set} (a: A), |- a = a.
 Axiom mk_comb: forall {A B: Set} (f1 f2: A -> B) (t1 t2: A), (|- f1 = f2) -> (|- t1 = t2) -> (|- f1 t1 = f2 t2).
 Axiom mk_abs: forall {A B: Set} (f1 f2: A -> B), (forall x, |- f1 x = f2 x) -> (|- f1 = f2).
 Axiom disch: forall b1 b2, (|- b2) -> (|- b1 ==> b2).
@@ -43,3 +43,29 @@ Definition conj_def (p1 p2: bool) : bool := Forall p, (p1 ==> (p2 ==> p)) ==> p.
 (* existential *)
 Definition exists_def {A: Set} (P: A -> bool) : bool := P (choice P). 
 
+(* some lemmas on equality *)
+Lemma mk_comb1 {A B: Set} (f1 f2: A -> B) (t: A): (|- f1 = f2) -> (|- f1 t = f2 t). 
+intros.
+apply mk_comb; [auto | apply eq_refl].
+Qed.
+
+Lemma mk_comb2 {A B: Set} (f: A -> B) (t1 t2: A): (|- t1 = t2) -> (|- f t1 = f t2). 
+intros.
+apply mk_comb; [apply eq_refl | auto].
+Qed.
+
+Lemma eq_trans {A: Set} (t1 t2 t3: A): (|- t1 = t2) -> (|- t2 = t3) -> (|- t1 = t3).
+intros.
+cut (|- (t1 = t2) = (t1 = t3)); intros.
+eapply eq_mp; [apply H1 | auto].
+apply mk_comb2; auto.
+Qed.
+
+Lemma eq_sym {A: Set} (t1 t2: A): (|- t1 = t2) -> (|- t2 = t1).
+intros.
+cut (|- (t1 = t1) = (t2 = t1)); intros. 
+eapply eq_mp; [apply H0 | apply eq_refl].
+apply mk_comb2 with (f := fun x => x = t1); auto.
+Qed.
+
+(* subst rules ??? *)
